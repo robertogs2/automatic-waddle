94374939e8b67681cb108de8fd77033a4201a58b
robertogs2 <roberm98a@gmail.com>
2019-11-07 19:17:31 -0600

    Added template for library

--- system/include/arduinocomms.h
+++ system/include/arduinocomms.h
@@ -5,4 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE
+
+typedef struct{
+	char buffer[BUFFER_SIZE];
+	char* filename;
+} arduino_comm_t;
+
+int init_arduino_comm(arduino_comm_t* interface, const char* filename);
+
+int sendchar(arduino_comm_t* interface, const char c);
+int sendstring(arduino_comm_t* interface, const char* string);
+int sendline(arduino_comm_t* interface, const char* line);
+
+char readchar(arduino_comm_t* interface);
+unsigned int readstring(arduino_comm_t* interface, char* result, unsigned int nbytes);
+unsigned int readline(arduino_comm_t* interface, char* result, unsigned int nbytes);
+
 #endif
